add8805140bfcc64ec3e41cf8c7e8ab272ab6966
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-04-16 01:17:48 +0430

    Change api urls

--- Programs/urls-api.py
+++ Programs/urls-api.py
@@ -3,6 +3,6 @@ from Programs import views
 
 app_name = 'Programs-api'
 urlpatterns = [
-    path('programs/v1/', views.ProgramApi.as_view()),
-    path('menu/v1/', views.MenuApi.as_view()),
+    path('v1/programs/', views.ProgramApi.as_view()),
+    path('v1/menu/', views.MenuApi.as_view()),
 ]
