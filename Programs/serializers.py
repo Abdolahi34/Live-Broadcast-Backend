018c915536b797316c0bbd304cca88e861df2df5
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-04-24 11:15:33 +0430

    api Validation
    move validation code from serializers to views & ...

--- Programs/serializers.py
+++ Programs/serializers.py
@@ -47,73 +47,25 @@ class ProgramSerializer(serializers.ModelSerializer):
         return obj.date_display
 
     def get_hours(self, obj):
-        return f"ساعت {obj.start_time.hour}:{obj.start_time.minute}"
+        return obj.time_display
 
     def get_link(self, obj):
         return obj.logo_link
 
     def get_logo(self, obj):
         request = self.context.get('request')
-        logo = obj.logo.url
-        return request.build_absolute_uri(logo)
+        return request.build_absolute_uri(obj.logo.url)
 
     def get_isLive(self, obj):
-        if obj.isLive:
-            base_datetime = datetime.datetime
-            start_time_datetime = base_datetime(base_datetime.now().year, base_datetime.now().month,
-                                                base_datetime.now().day, obj.start_time.hour,
-                                                obj.start_time.minute, obj.start_time.second, 0)
-            end_time_datetime = base_datetime(base_datetime.now().year, base_datetime.now().month,
-                                              base_datetime.now().day, obj.end_time.hour,
-                                              obj.end_time.minute, obj.end_time.second, 0)
-
-            if start_time_datetime <= base_datetime.now() <= end_time_datetime:
-                now_weekday = datetime.datetime.now().isoweekday()
-                if obj.datetime_type == 'regular':
-                    '''
-                    روز اول هفته در تقویم میلادی دوشنبه در نظر گرفته شده.
-                    در تقویم میلادی منظور از کد 0 یکشنبه می باشد.
-                    در تقویم شمسی منظور از کد 0 شنبه می باشد.
-                    '''
-                    if obj.regularly == 'daily':
-                        return True
-                    else:
-                        if obj.day_0:
-                            if now_weekday == 6:
-                                return True
-                        if obj.day_1:
-                            if now_weekday == 0:
-                                return True
-                        if obj.day_2:
-                            if now_weekday == 1:
-                                return True
-                        if obj.day_3:
-                            if now_weekday == 2:
-                                return True
-                        if obj.day_4:
-                            if now_weekday == 3:
-                                return True
-                        if obj.day_5:
-                            if now_weekday == 4:
-                                return True
-                        if obj.day_6:
-                            if now_weekday == 5:
-                                return True
-                else:
-                    for specified_date in obj.specified_date:
-                        if base_datetime.now().date() == specified_date:
-                            return True
-        return False
+        return obj.isLive
 
     def get_streams(self, obj):
         streams = {}
 
         if obj.is_voice_active:
             request = self.context.get('request')
-            player_background = obj.player_background.url
-            player_background_2 = request.build_absolute_uri(player_background)
+            player_background = request.build_absolute_uri(obj.player_background.url)
 
-            streams['image'] = {'url': player_background_2}
             streams['audio'] = {
                 'url': obj.voice_link,
                 'stats': {
@@ -121,6 +73,7 @@ class ProgramSerializer(serializers.ModelSerializer):
                     'type': obj.voice_stats_type
                 }
             }
+            streams['image'] = {'url': player_background}
         if obj.is_video_active:
             streams['video'] = {
                 'url': obj.video_link,
