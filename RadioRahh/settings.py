0005acda091ef18b1b679a096856da98cf8fd782
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-04-29 23:08:19 +0430

    Update .env file

--- RadioRahh/settings.py
+++ RadioRahh/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': '5432',
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
