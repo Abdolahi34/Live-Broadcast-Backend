9df20db3ccec2f4322b7c1f0e49c762e329f0b3b
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-04-29 23:14:42 +0430

    order program api with timestamp

--- Programs/views.py
+++ Programs/views.py
@@ -19,7 +19,19 @@ class ProgramApi(views.APIView):
     def get(self, request):
         try:
             queryset = models.Program.objects.all().filter(status='publish')
-            queryset.order_by('-isLive')
+
+            for program in queryset:
+                if program.datetime_type == 'weekly':
+                    program.timestamp_earliest = min(program.timestamps_start_weekly)
+                elif program.datetime_type == 'occasional':
+                    program.timestamp_earliest = min(program.timestamps_start_occasional)
+                else:
+                    timestamps_weekly_earliest = min(program.timestamps_start_weekly)
+                    timestamps_occasional_earliest = min(program.timestamps_start_occasional)
+                    program.timestamp_earliest = min(timestamps_weekly_earliest, timestamps_occasional_earliest)
+                queryset.filter(pk=program.pk).update(timestamp_earliest=program.timestamp_earliest)
+
+            queryset.order_by('-isLive', 'timestamp_earliest')
             serializer = serializers.ProgramSerializer(queryset, context={'request': request}, many=True)
             return response.Response(serializer.data, status=status.HTTP_200_OK)
         except:
