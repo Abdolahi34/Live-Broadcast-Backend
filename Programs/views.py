dfea68376c0f2d60d2de8c527f4209bf309b5ed9
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-04-30 07:36:20 +0430

    delete additional code

--- Programs/views.py
+++ Programs/views.py
@@ -17,33 +17,25 @@ from Programs import models, serializers
 
 class ProgramApi(views.APIView):
     def get(self, request):
-        try:
-            queryset = models.Program.objects.all().filter(status='publish')
-
-            for program in queryset:
-                if program.datetime_type == 'weekly':
-                    program.timestamp_earliest = min(program.timestamps_start_weekly)
-                elif program.datetime_type == 'occasional':
-                    program.timestamp_earliest = min(program.timestamps_start_occasional)
-                else:
-                    timestamps_weekly_earliest = min(program.timestamps_start_weekly)
-                    timestamps_occasional_earliest = min(program.timestamps_start_occasional)
-                    program.timestamp_earliest = min(timestamps_weekly_earliest, timestamps_occasional_earliest)
-                queryset.filter(pk=program.pk).update(timestamp_earliest=program.timestamp_earliest)
-
-            queryset.order_by('-isLive', 'timestamp_earliest')
-            serializer = serializers.ProgramSerializer(queryset, context={'request': request}, many=True)
-            return response.Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            return HttpResponseServerError()
+        queryset = models.Program.objects.all().filter(status='publish')
+        for program in queryset:
+            if program.datetime_type == 'weekly':
+                program.timestamp_earliest = min(program.timestamps_start_weekly)
+            elif program.datetime_type == 'occasional':
+                program.timestamp_earliest = min(program.timestamps_start_occasional)
+            else:
+                timestamps_weekly_earliest = min(program.timestamps_start_weekly)
+                timestamps_occasional_earliest = min(program.timestamps_start_occasional)
+                program.timestamp_earliest = min(timestamps_weekly_earliest, timestamps_occasional_earliest)
+            queryset.filter(pk=program.pk).update(timestamp_earliest=program.timestamp_earliest)
+        queryset.order_by('-isLive', 'timestamp_earliest')
+        serializer = serializers.ProgramSerializer(queryset, context={'request': request}, many=True)
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class MenuApi(views.APIView):
     def get(self, request):
-        try:
-            queryset = models.Menu.objects.all().order_by('num_order')
-            serializer_data = serializers.MenuSerializer(queryset, context={'request': request}, many=True)
-            serializer = {'menuItems': serializer_data.data}
-            return response.Response(serializer, status=status.HTTP_200_OK)
-        except:
-            return HttpResponseServerError()
+        queryset = models.Menu.objects.all().order_by('num_order')
+        serializer_data = serializers.MenuSerializer(queryset, context={'request': request}, many=True)
+        serializer = {'menuItems': serializer_data.data}
+        return response.Response(serializer, status=status.HTTP_200_OK)
