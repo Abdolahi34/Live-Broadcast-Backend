25ee4862c66c8155b6c9ece379bbac59eb8bce9a
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-04-30 23:04:13 +0430

    add name to api urls

--- Programs/urls-api.py
+++ Programs/urls-api.py
@@ -3,6 +3,6 @@ from Programs import views
 
 app_name = 'Programs-api'
 urlpatterns = [
-    path('v1/programs/', views.ProgramApi.as_view()),
-    path('v1/menu/', views.MenuApi.as_view()),
+    path('v1/programs/', views.ProgramApi.as_view(), name='programs'),
+    path('v1/menu/', views.MenuApi.as_view(), name='menu'),
 ]
