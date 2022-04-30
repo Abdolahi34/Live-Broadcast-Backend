6762c110d508643ec351fe55254f5e265eabe018
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-04-30 23:31:24 +0430

    Run Cronjob code with views.py

--- Programs/urls-api.py
+++ Programs/urls-api.py
@@ -3,6 +3,7 @@ from Programs import views
 
 app_name = 'Programs-api'
 urlpatterns = [
+    path('v1/create-programs-json/', views.create_programs_json, name='create_programs_json'),
     path('v1/programs/', views.ProgramApi.as_view(), name='programs'),
     path('v1/menu/', views.MenuApi.as_view(), name='menu'),
 ]
