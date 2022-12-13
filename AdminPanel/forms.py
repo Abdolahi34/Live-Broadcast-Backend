2e62aefc6540da363f36c3296eeca2562abcd0a1
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2022-12-13 11:43:44 +0330

    Few Small Changes

--- AdminPanel/forms.py
+++ AdminPanel/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from Programs import models
 
 
-class AdminProgramForm(forms.ModelForm):
+class AdminAddProgramForm(forms.ModelForm):
     class Meta:
         model = models.Program
         fields = '__all__'
--- Programs/urls-api.py
+++ Programs/urls-api.py
@@ -4,6 +4,7 @@ from Programs import views
 app_name = 'Programs-api'
 urlpatterns = [
     path('v1/create-programs-json/', views.create_programs_json, name='create_programs_json'),
+    path('v1/create-menu-json/', views.create_menu_json, name='create_menu_json'),
     path('v1/programs/', views.ProgramApi.as_view(), name='programs'),
     path('v1/menu/', views.MenuApi.as_view(), name='menu'),
 ]
