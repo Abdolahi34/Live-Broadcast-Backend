f07a05121c24d8483226f33093ded26df8768e6b
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2023-02-14 11:30:20 +0330

    Update models.py

--- Programs/models.py
+++ Programs/models.py
@@ -120,7 +120,7 @@ class Program(models.Model):
     is_audio_active = models.BooleanField(default=False, editable=False)
     is_video_active = models.BooleanField(default=False, editable=False)
     isLive = models.BooleanField(default=False, editable=False)
-    error_count = models.PositiveSmallIntegerField(default=0, editable=False),
+    error_count = models.PositiveSmallIntegerField(default=0, editable=False)
     creator = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, editable=False, verbose_name='سازنده',
                                 related_name='creator_program')
     latest_modifier = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, editable=False,
